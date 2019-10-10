72f5aba775167aa5104d380b54c47899b998ef5d
jCorralesLozano <jesselozano17@gmail.com>
2019-10-10 01:11:28 -0700

    Can switch subdomains when changing the question

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,42 +1,42 @@
 import React, {useState} from 'react';
 import {View, Text, FlatList, StyleSheet, Button, ScrollView} from 'react-native';
 import Answer from './Answer';
-import {getQuestionIndex} from '../hooks/HelperFunctions';
+import {getQuestionIndex, switchSubdomain} from '../hooks/HelperFunctions';
 
-const QuestionList = ({results, questionList}) => {
-    /* state variable to keep track of which question from 
-    a given subdomain should be displayed to the user */
-    const [questionIndex, setQuestionIndex] = useState(0);
+const QuestionList = ({results, questionIndex, questionList, navigation}) => {
+    /* state variable to keep track of which question should be displayed to the user */
+    const [index, setIndex] = useState(getQuestionIndex(results, results[questionIndex].QUESTION));
 
-    /* check to ensure question list is not empty when React first
-    renders the screen*/
-    if(questionList.length > 0 ){
-        return(
-            <ScrollView>
-                <Text>Subdomain: {questionList[questionIndex].SUBDOMAIN}</Text>
-                <Text>Comments: {questionList[questionIndex].COMMENTS}</Text>
-                <Text>Component: {questionList[questionIndex].COMPONENT}</Text>
-                <Text>Main: {questionList[questionIndex].MAIN}</Text>
-                <Text>Prompts: {questionList[questionIndex].PROMPTS}</Text>
-                <Text>Question: {questionList[questionIndex].QUESTION}</Text>
-                <Text>State Value: {questionIndex}</Text>
-                <Button
-                    title="Next"
-                    onPress={() => setQuestionIndex(questionIndex + 1)}
-                />
-                <Button
-                    title="Previous"
-                    onPress={() => setQuestionIndex(questionIndex - 1)}
-                />
-                <Answer 
-                    optionList={questionList[questionIndex].OPTIONS}
-                    contextIndex={getQuestionIndex(results, questionList[questionIndex].QUESTION)}
-                />
-            </ScrollView>
-        );
-    }else{
-        return null;
-    }
+    return(
+        <ScrollView>
+            <Text>Subdomain: {results[index].SUBDOMAIN}</Text>
+            <Text>Comments: {results[index].COMMENTS}</Text>
+            <Text>Component: {results[index].COMPONENT}</Text>
+            <Text>Main: {results[index].MAIN}</Text>
+            <Text>Prompts: {results[index].PROMPTS}</Text>
+            <Text>Question: {results[index].QUESTION}</Text>
+            <Text>State Value: {index}</Text>
+            <Button
+                title="Next"
+                onPress={() => setIndex(index + 1)}
+            />
+            <Button
+                title="Previous"
+                onPress={() => setIndex(index - 1)}
+            />
+            <Answer 
+                optionList={results[index].OPTIONS}
+                contextIndex={getQuestionIndex(results, results[index].QUESTION)}
+            />
+        </ScrollView>
+    );
+};
+
+QuestionList.navigationOptions = ({navigation}) => {
+    return{
+        title: "Question Huh?! <3",
+        headerTitle: 'headerTitle'
+    };
 };
 
 const styles = StyleSheet.create({
--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import useResults from '../hooks/useResults.js';
 import QuestionList from '../components/QuestionList.js';
-import {filterSubdomainArray} from '../hooks/HelperFunctions.js'
+import {getQuestionIndex, filterSubdomainArray} from '../hooks/HelperFunctions.js'
 
 
 var headerTitle = '';
@@ -10,30 +10,45 @@ var headerTitle = '';
 const QuestionScreen = ({navigation}) => {
     const [fetchQuestion, results, errorMessage] = useResults();
 
-    /* when this screen is navigate to, the navigation prop will have an additional param named 
+    /* when this screen is navigated to, the navigation prop will have an additional param named 
     'subdomain'. We retrieve the value to determine the questions from which subdomain to display */
     const subdomain = navigation.getParam('subdomain', 'something');
 
     /* update headerTitle so that it can be used in navigationOptions */
     headerTitle = subdomain;
-    console.log(`headerTitle: ${headerTitle}`);
 
     /* returns an array with questions pertinent to the given subdomain */
     const questionList = filterSubdomainArray(subdomain, results);
+    
+    /* For some reason, when React first renders this component, questionList is set to an empty array.
+    Thus, trying to access its values results in errors. Console logging its contents suggest that this component is rendered
+    twice, and it isn't until the second render that questionList actually contains data. I should research as to why */
+    if(questionList.length > 0){
+        /* retrieve the index of the first occurrence of the given subdomain */
+        const questionIndex = getQuestionIndex(results, questionList[0].QUESTION);
 
-    /* function will filter through and return an array of items with the same subdomain */
-    return(
-        <View style={styles.container}>
-            <Text>Subdomain: {subdomain}</Text>
-            <QuestionList results={results} questionList={questionList} />
-        </View>
-    );
+        /* function will filter through and return an array of items with the same subdomain */
+        return(
+            <View style={styles.container}>
+                <Text>Subdomain: {subdomain}</Text>
+                <QuestionList 
+                    results={results} 
+                    questionIndex={questionIndex} 
+                    questionList={questionList} 
+                    navigation={navigation}
+                />
+            </View>
+        );
+    }else{
+        return null;
+    }
+
 };
 
 QuestionScreen.navigationOptions = ({navigation}) => {
     return{
         title: "Question Huh?! <3",
-        headerTitle: headerTitle
+        headerTitle: 'Wellness Interview'
     };
 };
 
